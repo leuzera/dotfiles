3c3ba0e271cd3fd925625b4eca3d57de3d0ef101
Leonardo Fleury <fleuryleomoraes@gmail.com>
2018-09-19 19:25:18 -0300

    Limpando código

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #! /bin/zsh
 
-if command -v nvim > /dev/null;
-then
+if command -v nvim > /dev/null; then
     echo "Configuring neovim..."
     ln -sfi ~/.dotfiles/init.vim ~/.config/nvim/init.vim
     echo "\tdone."
@@ -9,18 +8,16 @@ else
     echo "Neovim not installed."
 fi
 
-if command -v zsh > /dev/null;
-then
+if command -v zsh > /dev/null; then
     echo "Configuring zsh..."
     ln -sfi ~/.dotfiles/zshenv_home ${HOME}/.zshenv
     . ${HOME}/.zshenv
 
-    if [[ -n ${ZDOTDIR} ]];
-    then
-        mkdir ${ZDOTDIR}/zsh
-        ln -sfi ~/.dotfiles/zshenv ${ZDOTDIR}/.zshenv
+    if [[ ! -d ${ZDOTDIR} ]]; then
+        mkdir ${ZDOTDIR}
     fi
 
+    ln -sfi ~/.dotfiles/zshenv ${ZDOTDIR}/.zshenv
     ln -sfi ~/.dotfiles/zshrc ${ZDOTDIR:-$HOME}/.zshrc
     ln -sfi ~/.dotfiles/zsh_aliases ${ZDOTDIR:-$HOME}/.zsh_aliases
     echo "\tdone."
@@ -28,8 +25,7 @@ else
     echo "Zsh not installed."
 fi
 
-if command -v tmux > /dev/null;
-then
+if command -v tmux > /dev/null; then
     echo "Configuring tmux..."
     ln -sfi ~/.dotfiles/tmux.conf ~/.tmux.conf
     echo "\tdone."
@@ -37,8 +33,7 @@ else
     echo "Tmux not installed."
 fi
 
-if [[ -d ${ZDOTDIR:-$HOME}/.zprezto ]];
-then
+if [[ -d ${ZDOTDIR:-$HOME}/.zprezto ]]; then
     echo "Configuring zpresto..."
     ln -sfi ~/.dotfiles/zprestorc ${ZDOTDIR:-$HOME}/.zpreztorc
     echo "\tdone."
