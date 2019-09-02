0d90b4dafa5ec058b2dd727c43b78a8537593761
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2019-09-02 10:55:49 -0300

    Simpler install script

--- install.sh
+++ install.sh
@@ -1,51 +1,33 @@
 #! /bin/zsh
 
-############ Neovim ###################
-if command -v nvim > /dev/null; then
-    echo "Configuring neovim..."
-    ln -sfi $(readlink -f ./config/nvim) ${HOME}/.config/nvim
-    echo "\tdone."
-else
-    echo "Neovim not installed."
+source /etc/os-release
+
+PKG_MNG="" # package management utility
+CMD_INSTALL="" # command to install program
+
+if $ID = "manjaro"; then 
+  PKG_MNG="pacman"
+  CMD_INSTALL="pacman -S"
+elif $ID = "ubuntu"; then
+  PKG_MNG="apt"
+  CMD_INSTALL="${PKG_MNG} -y install"
 fi
 
-################ Zsh ##################
-if command -v zsh > /dev/null; then
-    echo "Configuring zsh..."
-    ln -sf $(readlink -f ./zshenv) ${HOME}/.zshenv
-    . ${HOME}/.zshenv
+function install {
+  which $1 &> /dev/null
 
-    ln -sf $(readlink -f ./config/zsh) ${ZDOTDIR}
-    echo "\tdone."
-else
-    echo "Zsh not installed."
-fi
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    ${CMD_INSTALL} $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
 
-############### Alacritty ###############
-if command -v alacritty > /dev/null; then
-    echo "Configuring Alacritty..."
-    ln -sf $(readlink -f ./config/alacritty) ${HOME}/.config/alacritty
-    echo "\tdone."
-else
-    echo "Alacritty not installed."
-fi
-
-############### Tmux ####################
-if command -v tmux > /dev/null; then
-    echo "Configuring tmux..."
-    ln -sf $(readlink -f ./tmux.conf) ${HOME}/.tmux.conf
-    echo "\tdone."
-else
-    echo "Tmux not installed."
-fi
-
-############# Tint2 #####################
-if command -v tint2 > /dev/null; then
-    echo "Configuring tint2..."
-
-    ln -sf $(readlink -f ./config/tint2) ${HOME}/.config/tint2
-
-else
-    echo "Tint2 not installed."
-fi
+ln -sf $(readlink -f ./zshenv) ${HOME}/.zshenv && . ${HOME}/.zshenv
+ln -sf $(readlink -f ./config/nvim) ${HOME}/.config/nvim
+ln -sf $(readlink -f ./config/zsh) ${ZDOTDIR}
+ln -sf $(readlink -f ./config/alacritty) ${HOME}/.config/alacritty
+ln -sf $(readlink -f ./tmux.conf) ${HOME}/.tmux.conf
+ln -sf $(readlink -f ./config/tint2) ${HOME}/.config/tint2
 
